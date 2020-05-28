0580968e0e11954ac8fb6d29c38a504e21d98c2e
GearZer0 <61912722+GearZer0@users.noreply.github.com>
2020-05-28 15:40:51 +0800

    file and attachment path changes

--- auto.py
+++ auto.py
@@ -10,7 +10,7 @@ def createSSHClient(server, port, user, password):
     client.connect(server, port, user, password)
     return client
 
-def sendEmail(filename):
+def sendEmail(filenames):
     outlook = win32com.client.Dispatch('outlook.application')
     mail = outlook.CreateItem(0)
     mail.To = ' '
@@ -18,24 +18,37 @@ def sendEmail(filename):
     mail.Body = ' '
 #   mail.HTMLBody = '<h2>HTML Message body</h2>' #this field is optional
 
-    # To attach a file to the email (optional):
-    attachment  = filename
-    mail.Attachments.Add(attachment)
+    # To attach file to the email (optional):
+    for filename in filenames:
+        attachment  = filename
+        mail.Attachments.Add(attachment)
 
     mail.SentOnBehalfOfName = ' '
     mail.Send()
     print("Email sent ...")
 
 if __name__ == "__main__":
-    server = "IP address" # place the host name
+    try:
+        os.mkdir("SCP")
+    except:
+        pass
+    server = " " # place the host name
     port = "22" # place the port
-    user = "Username" # place the username
+    user = " " # place the username
     password = input("Enter Password: ")
     ssh = createSSHClient(server, port, user, password)
     scp = SCPClient(ssh.get_transport())
-    path = "/path/to/file" # place the file path that you want copied
-    filename = path.split('/')[-1] + ".csv"
-    scp.get(path, filename)
-    sendEmail(filename)
+    files = ['a','b'] # file names on server
+    local_files = []
+    for file_name in files:
+        path = "/path/to/folder/" + file_name # place the FOLDER path that you want copied
+        filename = path.split('/')[-1] + ".csv"
+        scp.get(path, 'SCP/' + filename)
+        if os.stat('SCP/' + filename).st_size == 0:
+            os.remove('SCP/' + filename) # zero length file
+            continue
+        local_files.append('SCP/' + filename)
+    sendEmail(local_files)
     scp.close()
-    os.remove(filename)
+    for file_name in local_files:
+        os.remove('SCP/' + file_name)
