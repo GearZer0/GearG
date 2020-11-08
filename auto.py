fc241218e705e08ed48471273c40c64a4a211a4d
GearZer0 <61912722+GearZer0@users.noreply.github.com>
2020-11-08 21:24:23 +0800

    Replace . with [dot] for url/domain

--- auto.py
+++ auto.py
@@ -3,6 +3,8 @@ from scp import SCPClient
 import win32com.client
 import os
 from getpass import getpass
+import re
+
 
 def createSSHClient(server, port, user, password):
     try:
@@ -15,6 +17,7 @@ def createSSHClient(server, port, user, password):
         print("Wrong password")
         quit()
 
+
 def sendEmail(filenames):
     outlook = win32com.client.Dispatch('outlook.application')
     mail = outlook.CreateItem(0)
@@ -25,33 +28,38 @@ def sendEmail(filenames):
 
     # To attach file to the email (optional):
     for filename in filenames:
-        attachment  = filename
+        attachment = filename
         mail.Attachments.Add(attachment)
 
     mail.SentOnBehalfOfName = ''
     mail.Send()
-    print("Email sent ...") 
+    print("Email sent ...")
+
 
 if __name__ == "__main__":
     try:
-        os.mkdir("SCP") # folder name
+        os.mkdir("SCP")  # folder name
     except:
         pass
-    server = "" # place the host name
-    port = "" # place the port
-    user = "" # place the username
+    server = ""  # place the host name
+    port = ""  # place the port
+    user = ""  # place the username
     password = getpass()
     # password = ""
     ssh = createSSHClient(server, port, user, password)
     scp = SCPClient(ssh.get_transport())
-    files = [''] # file names on server
+    files = ['']  # file names on server
+    for i in range(len(files)):
+        if re.findall(r'[0-255].[0-255].[0-255].[0-255]', files[i]) == 0:
+            # applying replacement job
+            files[i] = files[i].replace('.', '[dot]')
     local_files = []
     for file_name in files:
-        path = "" + file_name # place the FOLDER path that you want copied
+        path = "" + file_name  # place the FOLDER path that you want copied
         filename = path.split('/')[-1] + ".csv"
         scp.get(path, os.getcwd() + '/SCP/' + filename)
         if os.stat(os.getcwd() + '/SCP/' + filename).st_size == 0:
-            os.remove(os.getcwd() + '/SCP/' + filename) # zero length file
+            os.remove(os.getcwd() + '/SCP/' + filename)  # zero length file
             continue
         local_files.append(os.getcwd() + '/SCP/' + filename)
     sendEmail(local_files)
